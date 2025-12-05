da445e59b4ea0da1b8525d77667c9114f62abfc4
Saketh Reddy <kk8429@srmist.edu.in>
2025-12-05 12:24:51 +0530

    chnages done

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 # Enable CORS for localhost:3000
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://gst-staging-nextjs-589889616484.us-central1.run.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
